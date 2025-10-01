de45e434ff089294a0b6fac530d7f24bcbc86a27
Mohammad Atikuzzaman <akash203037@gmail.com>
2025-10-01 22:31:59 +0600

    protype added for objects method

--- day2/user.js
+++ day2/user.js
@@ -1,7 +1,24 @@
 function User(name, email) {
   this.name = name;
   this.email = email;
+
+// This is inefficient, so we would not use this type of method
+//   this.greet = function(){
+//     console.log(`Hi, I am ${this.name}`);
+//   }
+
 }
 
+
+
+// This is the best practice for make method for object. 
+User.prototype.greet = function(){
+    console.log(`Hi, I am ${this.name}, my email is ${this.email}`);
+}
+
+
+
 const akash = new User("Akash", "akash203037@gmail.com")
-console.log(akash);
+// console.log(akash);
+
+akash.greet()
