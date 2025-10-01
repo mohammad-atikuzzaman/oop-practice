ef0e2e4e9af08ac1fd58f52ea27053a65b91ee5f
Mohammad Atikuzzaman <akash203037@gmail.com>
2025-10-01 23:04:10 +0600

    we can return value from method of the object that we have created useing constractore

--- day2/user.js
+++ day2/user.js
@@ -2,23 +2,34 @@ function User(name, email) {
   this.name = name;
   this.email = email;
 
-// This is inefficient, so we would not use this type of method
-//   this.greet = function(){
-//     console.log(`Hi, I am ${this.name}`);
-//   }
-
+  // This is inefficient, so we would not use this type of method
+  //   this.greet = function(){
+  //     console.log(`Hi, I am ${this.name}`);
+  //   }
 }
 
+// This is the best practice for make method for object.
+User.prototype.greet = function () {
+  console.log(`Hi, I am ${this.name}, my email is ${this.email}`);
+};
 
-
-// This is the best practice for make method for object. 
-User.prototype.greet = function(){
-    console.log(`Hi, I am ${this.name}, my email is ${this.email}`);
-}
-
-
-
-const akash = new User("Akash", "akash203037@gmail.com")
+const akash = new User("Akash", "akash203037@gmail.com");
+const atik = new User("Atikuzzaman", "mdatikuzzaman1331@gmail.com");
 // console.log(akash);
 
-akash.greet()
+// akash.greet()
+// atik.greet()
+
+function Student(name, role) {
+  this.name = name;
+  this.role = role;
+}
+
+// We can return the value from 
+Student.prototype.greet = function(){
+    return `Hi, I am ${this.name}, my role is ${this.role}`
+}
+
+
+const student1 = new Student("Md Akash",12)
+console.log(student1.greet());
