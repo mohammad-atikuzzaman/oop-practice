fca053ad06571af5619debc94c64afdd6dc773ca
Mohammad Atikuzzaman <akash203037@gmail.com>
2025-10-02 11:49:28 +0600

    soulution added

--- day3/bank-account.js
+++ day3/bank-account.js
@@ -12,6 +12,15 @@ class BankAccount {
   }
 }
 
-const akashsAccount = new BankAccount("Akash", 80)
-console.log(akashsAccount.deposit(20));
-console.log(akashsAccount.withdraw(50));
+const akashsAccount = new BankAccount("Akash", 80);
+
+// console.log(akashsAccount.deposit(20));
+// console.log(akashsAccount.withdraw(50));
+
+const { deposit, withdraw } = akashsAccount;
+
+
+// If we destructure the methods from object. we need to use .call() , and on the first parameter we have to use the object name, and others parameter
+
+// console.log(deposit.call(akashsAccount, 20));
+// console.log(withdraw.call(akashsAccount, 50));
