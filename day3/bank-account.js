0097d15595b041f49867f155e6a7edafe1eee664
Mohammad Atikuzzaman <akash203037@gmail.com>
2025-10-02 11:59:37 +0600

    Fix porblem on account balance

--- day3/bank-account.js
+++ day3/bank-account.js
@@ -4,11 +4,17 @@ class BankAccount {
     this.balance = balance;
   }
   deposit(amount = 0) {
-    return this.balance + amount;
+    return amount <= 0
+      ? "Deposit amount must be positive"
+      : (this.balance += amount);
   }
 
   withdraw(amount = 0) {
-    return this.balance - amount;
+    return amount <= 0
+      ? "Withdraw amount must be positive"
+      : this.balance < amount
+      ? "Inefficient Balance"
+      : (this.balance -= amount);
   }
 }
 
@@ -19,8 +25,7 @@ const akashsAccount = new BankAccount("Akash", 80);
 
 const { deposit, withdraw } = akashsAccount;
 
-
 // If we destructure the methods from object. we need to use .call() , and on the first parameter we have to use the object name, and others parameter
 
-// console.log(deposit.call(akashsAccount, 20));
-// console.log(withdraw.call(akashsAccount, 50));
+console.log(deposit.call(akashsAccount, 20));
+console.log(withdraw.call(akashsAccount, 50));
