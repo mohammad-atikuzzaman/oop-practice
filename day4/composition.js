294e6702ed9f75402b32020e5040b894d148c4d7
Mohammad Atikuzzaman <akash203037@gmail.com>
2025-10-04 12:00:43 +0600

    anoterher practice for understnding componsition

--- day4/composition.js
+++ day4/composition.js
@@ -1,6 +1,12 @@
-class Sward {
+class Sword {
   use() {
     return "Slashes with a sward";
+  }
+}
+
+class MagicStaff {
+  use() {
+    return "casts a lightning bolt!";
   }
 }
 
@@ -10,7 +16,7 @@ class Bow {
   }
   use() {
     this.arrow -= 1;
-    return `Shot an Arrow, remaining arrow ${this.arrow.}`;
+    return `Shot an Arrow, remaining arrow ${this.arrow}`;
   }
 }
 
@@ -23,8 +29,28 @@ class Character {
   attack() {
     return `${this.name} ${this.weapon.use()}`;
   }
+
+  changeWeapon(newWeapon) {
+    this.weapon = newWeapon;
+    return `${this.name} switched to a new weapon.`;
+  }
 }
 
-const c1 = new Character("Thor", new Bow());
-console.log(c1);
-console.log(c1.attack());
+// const c1 = new Character("Thor", new Bow());
+// console.log(c1);
+// console.log(c1.attack());
+
+const sword = new Sword();
+const bow = new Bow();
+const staff = new MagicStaff();
+
+const warrior = new Character("Leonidas", sword);
+const archer = new Character("Legolas", bow);
+const mage = new Character("Gandalf", staff);
+
+console.log(warrior.attack());
+console.log(archer.attack());
+console.log(mage.attack());
+
+console.log(warrior.changeWeapon(bow));
+console.log(warrior.attack());
