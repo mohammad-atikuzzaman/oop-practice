4b7b0921b8f47fcda9484802f8dfbaae944b3bfe
Mohammad Atikuzzaman <akash203037@gmail.com>
2025-10-07 16:08:41 +0600

    another abstration practice done

--- day8/abstraction.js
+++ day8/abstraction.js
@@ -14,19 +14,73 @@ class Vehicle {
 }
 
 class Car extends Vehicle {
-
   start() {
     return `${this.brand} car is started`;
   }
 }
 
 class ElectricCar extends Vehicle {
-    start() {
-        console.log(`${this.brand} electric car is starting silently`);
-    }
+  start() {
+    console.log(`${this.brand} electric car is starting silently`);
+  }
 }
 
 const car1 = new Car("Toyota");
-const car2 = new ElectricCar("Tesla")
-console.log(car1);
-console.log(car1.start());
+const car2 = new ElectricCar("Tesla");
+// console.log(car1);
+// console.log(car1.start());
+
+// ===============================================================================================
+//    Real word implementation
+// ===============================================================================================
+
+class Payment {
+  constructor(amount) {
+    if (this.constructor === Payment) {
+      throw new Error("Abstract Class");
+    }
+    this.amount = amount;
+  }
+
+  pay() {
+    throw new Error("Implement pay on your payment method");
+  }
+  mobileRecharge() {
+    throw new Error("Implement mobile recharge system on you payment method");
+  }
+  sendMoney() {
+    return `${this.amount} is sending`;
+  }
+}
+
+class BkashPay extends Payment {
+  pay() {
+    return `Paying ${this.amount} tk using Bkash`;
+  }
+  mobileRecharge() {
+    return `${this.amount} of mobile recharge is sent to the operator`;
+  }
+}
+
+class CashPay extends Payment {
+  pay() {
+    return `Paying ${this.amount} tk using cash`;
+  }
+  mobileRecharge() {
+    return `Hey Shop keeper brother please give me ${this.amount} of mobile recharge`;
+  }
+}
+
+class CardPay extends Payment {
+  pay() {
+    return `Paying ${this.amount} tk using Card`;
+  }
+}
+
+const p1 = new BkashPay(200);
+const p2 = new CashPay(200);
+const p3 = new CardPay(200);
+
+// console.log(p1, p2, p3);
+
+[p1, p2, p3].forEach(i=> console.log(i.mobileRecharge()))
